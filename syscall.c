2f4690b73189cda3f71d0a688a26b9491d33689b
Takahiro Shinagawa <shina@ecc.u-tokyo.ac.jp>
2018-11-07 14:24:20 +0900

    add syscall.c

--- syscall.c
+++ syscall.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-#include "vm.h"
 #include "syscall.h"
 #include "linux.h"
 
-int
-brk(void *addr)
+int brk(process_t *process, mm_gvirt_t newbrk)
 {
-	printf("brk: %p\n", addr);
-	return -1;
+	if (newbrk < process->mm.heap_start)
+		return process->mm.heap_end;
+	mm_expand_heap(&process->mm, newbrk);
+	printf("brk: %lx\n", newbrk);
+	return newbrk;
 }
 
-int
-handle_syscall(vm_t *vm, uint64_t sysnum, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6)
+int handle_syscall(process_t *process, uint64_t sysnum, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5, uint64_t arg6)
 {
-	printf("syscall(%ld)(%lx,%lx,%lx)\n", sysnum, arg1, arg2, arg3);
-	switch (sysnum) {
-		case LINUX_sys_brk:
-		return brk((void *)arg1);
+	printf("%s(%lx,%lx,%lx,%lx,%lx,%lx)\n", syscall_name[sysnum], arg1, arg2, arg3, arg4, arg5, arg6);
+	switch (sysnum)
+	{
+	case LINUX_SYS_brk:
+		return brk(process, arg1);
 
-	case LINUX_sys_getuid:
-	case LINUX_sys_getgid:
-	case LINUX_sys_geteuid:
-	case LINUX_sys_getegid:
+	case LINUX_SYS_getuid:
+	case LINUX_SYS_getgid:
+	case LINUX_SYS_geteuid:
+	case LINUX_SYS_getegid:
 		return 1000;
 
 	default:
